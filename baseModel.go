6228fcf138c472a2169d69d046da79d7c4da0430
sid314 <sidaa314@gmail.com>
2025-08-17 17:19:25 +0530

    listUpdateNeededMsg is now passed to list

--- baseModel.go
+++ baseModel.go
@@ -71,6 +71,8 @@ func (b baseModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	var cmds []tea.Cmd
 	switch message := msg.(type) {
+	case listUpdateNeededMsg:
+		passToList = true
 	case tea.KeyMsg:
 		active = true
 		switch message.String() {
