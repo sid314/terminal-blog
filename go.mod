7eb0149f462dcbbc52798765c33dc76d3a58aac3
sid314 <sidaa314@gmail.com>
2025-09-10 22:06:36 +0530

    started to work on keeping track of multiple tea.Programs

--- go.mod
+++ go.mod
@@ -7,22 +7,34 @@ require (
 	github.com/charmbracelet/bubbletea v1.3.6
 	github.com/charmbracelet/glamour v0.10.0
 	github.com/charmbracelet/lipgloss v1.1.1-0.20250404203927-76690c660834
+	github.com/charmbracelet/log v0.4.2
+	github.com/charmbracelet/ssh v0.0.0-20250429213052-383d50896132
+	github.com/charmbracelet/wish v1.4.7
 	github.com/davecgh/go-spew v1.1.1
 	github.com/fsnotify/fsnotify v1.9.0
 )
 
 require (
 	github.com/alecthomas/chroma/v2 v2.14.0 // indirect
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
+	github.com/charmbracelet/keygen v0.5.3 // indirect
 	github.com/charmbracelet/x/ansi v0.9.3 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13 // indirect
+	github.com/charmbracelet/x/conpty v0.1.0 // indirect
+	github.com/charmbracelet/x/errors v0.0.0-20240508181413-e8d8b6e2de86 // indirect
 	github.com/charmbracelet/x/exp/slice v0.0.0-20250327172914-2fdc97757edf // indirect
+	github.com/charmbracelet/x/input v0.3.4 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/charmbracelet/x/termios v0.1.0 // indirect
+	github.com/charmbracelet/x/windows v0.2.0 // indirect
+	github.com/creack/pty v1.1.21 // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -38,8 +50,9 @@ require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/yuin/goldmark v1.7.8 // indirect
 	github.com/yuin/goldmark-emoji v1.0.5 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
-	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/net v0.36.0 // indirect
 	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/term v0.31.0 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,188 @@
 package main
 
 import (
-	"log"
+	"context"
+	"net"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
+	"github.com/muesli/termenv"
 )
 
-func main() {
+const (
+	host = "localhost"
+	port = "6942"
+)
+
+type app struct {
+	server *ssh.Server
+	progs  []*tea.Program
+}
+
+func (a app) send(msg tea.Msg) {
+	for _, p := range a.progs {
+		go p.Send(msg)
+	}
+}
+
+func newApp() *app {
+	a := new(app)
+	s, err := wish.NewServer(wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(bubbletea.MiddlewareWithProgramHandler(a.ProgramHandler, termenv.ANSI256),
+			activeterm.Middleware(),
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+	a.server = s
+	return a
+}
+
+func (a *app) Start() {
+	var err error
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = a.server.ListenAndServe(); err != nil {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := a.server.Shutdown(ctx); err != nil {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+func (a *app) ProgramHandler(s ssh.Session) *tea.Program {
 	var dump *os.File
-	a := 1
 	os.Setenv("DEBUG", "true")
 	if _, ok := os.LookupEnv("DEBUG"); ok {
-		a = 2
 		var err error
 		dump, err = os.OpenFile("messages.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
 		if err != nil {
 			os.Exit(1)
 		}
 	}
-	println(a)
-	m, err := initialBaseModel(dump)
-	if err != nil {
-		log.Fatal(err)
-	}
-	p := tea.NewProgram(m, tea.WithAltScreen())
-	// p := tea.NewProgram(initialList(), tea.WithAltScreen())
-	go func() {
-		checkFolderUpdates(p)
-	}()
-	_, err = p.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	model, _ := initialBaseModel(dump)
+
+	p := tea.NewProgram(model, bubbletea.MakeOptions(s)...)
+	a.progs = append(a.progs, p)
+
+	return p
 }
+
+func main() {
+	app := newApp()
+	app.Start()
+}
+
+//
+// func main() {
+// 	// var dump *os.File
+// 	// a := 1
+// 	// os.Setenv("DEBUG", "true")
+// 	// if _, ok := os.LookupEnv("DEBUG"); ok {
+// 	// 	a = 2
+// 	// 	var err error
+// 	// 	dump, err = os.OpenFile("messages.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
+// 	// 	if err != nil {
+// 	// 		os.Exit(1)
+// 	// 	}
+// 	// }
+// 	// println(a)
+// 	// m, err := initialBaseModel(dump)
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	// p := tea.NewProgram(m, tea.WithAltScreen())
+// 	// // p := tea.NewProgram(initialList(), tea.WithAltScreen())
+// 	// go func() {
+// 	// 	checkFolderUpdates(p)
+// 	// }()
+// 	// _, err = p.Run()
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	s, err := wish.NewServer(
+// 		wish.WithAddress(net.JoinHostPort(host, port)),
+// 		wish.WithHostKeyPath(".ssh/id_ed25519"),
+// 		wish.WithMiddleware(
+// 			bubbletea.Middleware(teaHandler),
+// 			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+// 			logging.Middleware(),
+// 		),
+// 	)
+// 	if err != nil {
+// 		log.Error("Could not start server", "error", err)
+// 	}
+//
+// 	done := make(chan os.Signal, 1)
+// 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+// 	log.Info("Starting SSH server", "host", host, "port", port)
+// 	go func() {
+// 		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+// 			log.Error("Could not start server", "error", err)
+// 			done <- nil
+// 		}
+// 	}()
+//
+// 	<-done
+// 	log.Info("Stopping SSH server")
+// 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+// 	defer func() { cancel() }()
+// 	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+// 		log.Error("Could not stop server", "error", err)
+// 	}
+// }
+//
+// func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+// 	// This should never fail, as we are using the activeterm middleware.
+// 	s.Pty()
+//
+// 	// When running a Bubble Tea app over SSH, you shouldn't use the default
+// 	// lipgloss.NewStyle function.
+// 	// That function will use the color profile from the os.Stdin, which is the
+// 	// server, not the client.
+// 	// We provide a MakeRenderer function in the bubbletea middleware package,
+// 	// so you can easily get the correct renderer for the current session, and
+// 	// use it to create the styles.
+// 	// The recommended way to use these styles is to then pass them down to
+// 	// your Bubble Tea model.
+//
+// 	var dump *os.File
+// 	a := 1
+// 	os.Setenv("DEBUG", "true")
+// 	if _, ok := os.LookupEnv("DEBUG"); ok {
+// 		a = 2
+// 		var err error
+// 		dump, err = os.OpenFile("messages.log", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
+// 		if err != nil {
+// 			os.Exit(1)
+// 		}
+// 	}
+// 	println(a)
+// 	m, err := initialBaseModel(dump)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	return m, []tea.ProgramOption{tea.WithAltScreen()}
+// }
